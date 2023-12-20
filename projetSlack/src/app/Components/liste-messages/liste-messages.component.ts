b8bfdefaca415006acf7405f4f28b7a51b0b8782
Moussa Mimoune <moussa.ehm@gmail.com>
2023-12-20 13:42:51 +0100

    Correctif

---
+++ projetSlack/src/app/Components/liste-messages/liste-messages.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-messages',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-messages.component.html',
+  styleUrl: './liste-messages.component.css'
+})
+export class ListeMessagesComponent {
+
+
+  
+}
