b16099496798d9c2d6b7ce00e63f939d081c7484
Moussa Mimoune <moussa.ehm@gmail.com>
2023-12-20 12:27:20 +0100

    Page Connexion

---
+++ projetSlack/src/app/Services/fetcher-user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../Models/User.model';
+import { UserPost } from '../Models/UserPost.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetcherUserService {
+
+  constructor(private http: HttpClient) { }
+
+  get() {
+    return this.http.get<User[]>('http://localhost:8080/utilisateurs');
+  }
+  
+  getById(id: number) {
+    return this.http.get<User>('http://localhost:8080/utilisateurs/' + id);
+  }
+
+  post(user: UserPost) {
+    return this.http.post('http://localhost:8080/utilisateurs', user);
+  }
+
+  delete(id: number) {
+    return this.http.delete('http://localhost:8080/utilisateurs/' + id);
+  }
+
+  update(user: User) {
+    return this.http.put('http://localhost:8080/utilisateurs/' + user.id, user);
+  }
+}
